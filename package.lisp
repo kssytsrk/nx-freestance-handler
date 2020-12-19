3b6328d1d9259d7d759b9b140602cf447c1e6b68
kssytsrk <kassy@dismail.de>
2020-12-19 22:18:34 +0200

    updated README.md and documentation in package.lisp

--- package.lisp
+++ package.lisp
@@ -21,20 +21,7 @@
 
 (uiop:define-package :nx-freestance-handler
     (:use :common-lisp :nyxt)
-  (:documentation "A redirector from mainstream websites to their privacy-supporting mirrors for the Nyxt browser.
-
-Invidious is an alternative front-end to Youtube that uses copylefted libre software and respects user's privacy.
-
-To turn the handler on, add something like this to your init file:
-\"(asdf:load-system :nx-freestance-handler)
-
-  (define-configuration buffer
-    ((request-resource-hook
-      (reduce #'hooks:add-hook
-              (list nx-freestance-handler:invidious-handler)
-              :initial-value %slot-default))))\"
-
-By default, this handler redirects from youtube.com to the healthiest (i.e, with best uptime) instance available, but if you prefer a particular one, it can be set with SET-PREFERRED-INVIDIOUS-INSTANCE Nyxt command, or in your init file with `(setf nx-freestance-handler:*preferred-invidious-instance* \"example.org\")`.")
+  (:documentation "A redirector from mainstream websites to their privacy-supporting mirrors for the Nyxt browser.")
   (:export :*preferred-invidious-instance*
            :*preferred-teddit-instance*
            :*preferred-bibliogram-instance*
